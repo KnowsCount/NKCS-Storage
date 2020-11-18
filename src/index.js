f665c2e1cde0db61085d194208add83e768d2db1
KnowsCount <knowscount@gmail.com>
2020-11-18 09:58:28 +0800

    beta.18112020@indentation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
  * @Date: 2018-04-01 23:47:56
  * @Author: KnowsCount
  * @Github: https://github.com/KnowsCount/NKCS-Storage
- * @LastEditTime: 2020-11-18 03:17:09
+ * @LastEditTime: 2020-11-18 09:52:28
  * @FilePath: /ManagingSystem/src/index.js
- * 
+ *
  *                        _oo0oo_
  *                       o8888888o
  *                       88" . "88
@@ -24,44 +24,44 @@
  *          \  \ `_.   \_ __\ /__ _/   .-` /  /
  *      =====`-.____`.___ \_____/___.-`___.-'=====
  *                        `=---='
- * 
- * 
+ *
+ *
  *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * 
+ *
  *            佛祖保佑       永不宕机     永无BUG
  */
 
-import './index.html';
-import './index.less';
-import dva from 'dva';
-import {browserHistory} from 'dva/router';
+import "./index.html";
+import "./index.less";
+import dva from "dva";
+import { browserHistory } from "dva/router";
 
 // 1. Initialize
 const app = dva({
-	history: browserHistory
+	history: browserHistory,
 });
 
 // 2. Plugins
 //app.use({});
 
 // 3. Model
-app.model(require('./models/home'));
-app.model(require('./models/orders'));
+app.model(require("./models/home"));
+app.model(require("./models/orders"));
 /*app.model(require('./models/stocks'));*/
-app.model(require('./models/storage'));
+app.model(require("./models/storage"));
 /*app.model(require('./models/funds'));*/
-app.model(require('./models/manage'));
-app.model(require('./models/systemUser'));
-app.model(require('./models/customers'));
-app.model(require('./models/products'));
-app.model(require('./models/suppliers'));
-app.model(require('./models/settlement'));
-app.model(require('./models/resource'));
-app.model(require('./models/customerBills'));
-app.model(require('./models/supplierBills'));
+app.model(require("./models/manage"));
+app.model(require("./models/systemUser"));
+app.model(require("./models/customers"));
+app.model(require("./models/products"));
+app.model(require("./models/suppliers"));
+app.model(require("./models/settlement"));
+app.model(require("./models/resource"));
+app.model(require("./models/customerBills"));
+app.model(require("./models/supplierBills"));
 
 // 4. Router
-app.router(require('./router'));
+app.router(require("./router"));
 
 // 5. Start
-app.start('#root');
+app.start("#root");
--- src/router.js
+++ src/router.js
@@ -3,89 +3,127 @@
  * @Date: 2018-04-01 23:47:56
  * @Author: KnowsCount
  * @Github: https://github.com/KnowsCount/NKCS-Storage
- * @LastEditTime: 2020-11-18 03:17:34
+ * @LastEditTime: 2020-11-18 09:57:05
  * @FilePath: /ManagingSystem/src/router.js
- * 
- * _______________#########_______________________ 
- * ______________############_____________________ 
- * ______________#############____________________ 
- * _____________##__###########___________________ 
- * ____________###__######_#####__________________ 
- * ____________###_#######___####_________________ 
- * ___________###__##########_####________________ 
- * __________####__###########_####_______________ 
- * ________#####___###########__#####_____________ 
- * _______######___###_########___#####___________ 
- * _______#####___###___########___######_________ 
- * ______######___###__###########___######_______ 
- * _____######___####_##############__######______ 
- * ____#######__#####################_#######_____ 
- * ____#######__##############################____ 
- * ___#######__######_#################_#######___ 
- * ___#######__######_######_#########___######___ 
- * ___#######____##__######___######_____######___ 
- * ___#######________######____#####_____#####____ 
- * ____######________#####_____#####_____####_____ 
- * _____#####________####______#####_____###______ 
- * ______#####______;###________###______#________ 
- * ________##_______####________####______________ 
+ *
+ * _______________#########_______________________
+ * ______________############_____________________
+ * ______________#############____________________
+ * _____________##__###########___________________
+ * ____________###__######_#####__________________
+ * ____________###_#######___####_________________
+ * ___________###__##########_####________________
+ * __________####__###########_####_______________
+ * ________#####___###########__#####_____________
+ * _______######___###_########___#####___________
+ * _______#####___###___########___######_________
+ * ______######___###__###########___######_______
+ * _____######___####_##############__######______
+ * ____#######__#####################_#######_____
+ * ____#######__##############################____
+ * ___#######__######_#################_#######___
+ * ___#######__######_######_#########___######___
+ * ___#######____##__######___######_____######___
+ * ___#######________######____#####_____#####____
+ * ____######________#####_____#####_____####_____
+ * _____#####________####______#####_____###______
+ * ______#####______;###________###______#________
+ * ________##_______####________####______________
  */
 
-import React, {PropTypes} from 'react';
-import {Router, Route, IndexRoute, Link} from 'dva/router';
-import HomePage from './routes/HomePage/HomePage';
-import IndexPage from './routes/IndexPage/IndexPage';
-import Orders from './routes/Orders/Orders';
-import Storage from './routes/Storage/Storage';
-import Stock from './routes/Stock/Stock';
-import Funds from './routes/Funds/Funds';
-import Resource from './routes/Resource/Resource';
-import Settlement from './routes/Settlement/Settlement';
-import Bills from './routes/Bills/Bills';
-import CustomerBills from './routes/CustomerBills/CustomerBills';
-import SupplierBills from './routes/SupplierBills/SupplierBills';
-import Manage from './routes/Manage/Manage';
-import Customers from './routes/Customers/Customers';
-import Products from './routes/Products/Products';
-import Suppliers from './routes/Suppliers/Suppliers';
-import {requireAuth} from './utils/webSessionUtils';
+import React, { PropTypes } from "react";
+import { Router, Route, IndexRoute, Link } from "dva/router";
+import HomePage from "./routes/HomePage/HomePage";
+import IndexPage from "./routes/IndexPage/IndexPage";
+import Orders from "./routes/Orders/Orders";
+import Storage from "./routes/Storage/Storage";
+import Stock from "./routes/Stock/Stock";
+import Funds from "./routes/Funds/Funds";
+import Resource from "./routes/Resource/Resource";
+import Settlement from "./routes/Settlement/Settlement";
+import Bills from "./routes/Bills/Bills";
+import CustomerBills from "./routes/CustomerBills/CustomerBills";
+import SupplierBills from "./routes/SupplierBills/SupplierBills";
+import Manage from "./routes/Manage/Manage";
+import Customers from "./routes/Customers/Customers";
+import Products from "./routes/Products/Products";
+import Suppliers from "./routes/Suppliers/Suppliers";
+import { requireAuth } from "./utils/webSessionUtils";
 
-export default function ({history}) {
-    return (
-        <Router history={history}>
-            <Route path="/" component={HomePage}>
+export default function ({ history }) {
+	return (
+		<Router history={history}>
+			<Route path="/" component={HomePage}>
+				<IndexRoute component={IndexPage} />
 
-                <IndexRoute component={IndexPage}/>
+				<Route path="/orders" onEnter={requireAuth} component={Orders}>
+					<Route
+						path="/orders/*"
+						onEnter={requireAuth}
+						component={Orders}
+					/>
+				</Route>
 
-                <Route path="/orders" onEnter={requireAuth} component={Orders}>
-                    <Route path="/orders/*" onEnter={requireAuth} component={Orders}/>
-                </Route>
+				<Route
+					path="/storage"
+					onEnter={requireAuth}
+					component={Storage}
+				/>
 
-                <Route path="/storage" onEnter={requireAuth} component={Storage}/>
-
-               {/* <Route path="/stock" component={Stock}/>
+				{/* <Route path="/stock" component={Stock}/>
 
                 <Route path="/funds" component={Funds}/>*/}
 
-                <Route path="/resource" onEnter={requireAuth} component={Resource}/>
+				<Route
+					path="/resource"
+					onEnter={requireAuth}
+					component={Resource}
+				/>
 
-				<Route path="/settlement" onEnter={requireAuth} component={Settlement}/>
+				<Route
+					path="/settlement"
+					onEnter={requireAuth}
+					component={Settlement}
+				/>
 
-				<Route path="/bills" onEnter={requireAuth} component={Bills}/>
+				<Route path="/bills" onEnter={requireAuth} component={Bills} />
 				<Route component={Bills}>
-					<Route path="/customerBills" onEnter={requireAuth} component={CustomerBills}/>
-					<Route path="/supplierBills" onEnter={requireAuth} component={SupplierBills}/>
+					<Route
+						path="/customerBills"
+						onEnter={requireAuth}
+						component={CustomerBills}
+					/>
+					<Route
+						path="/supplierBills"
+						onEnter={requireAuth}
+						component={SupplierBills}
+					/>
 				</Route>
 
-                <Route path="/manage" onEnter={requireAuth} component={Manage}/>
+				<Route
+					path="/manage"
+					onEnter={requireAuth}
+					component={Manage}
+				/>
 
-                <Route component={Manage}>
-                    <Route path="/customer" onEnter={requireAuth} component={Customers}/>
-                    <Route path="/product" onEnter={requireAuth} component={Products}/>
-                    <Route path="/supplier" onEnter={requireAuth} component={Suppliers}/>
-                </Route>
-
-            </Route>
-        </Router>
-    );
-};
+				<Route component={Manage}>
+					<Route
+						path="/customer"
+						onEnter={requireAuth}
+						component={Customers}
+					/>
+					<Route
+						path="/product"
+						onEnter={requireAuth}
+						component={Products}
+					/>
+					<Route
+						path="/supplier"
+						onEnter={requireAuth}
+						component={Suppliers}
+					/>
+				</Route>
+			</Route>
+		</Router>
+	);
+}
