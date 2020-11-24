52e6b4a5460e4cbb265b7e569e9e5b33d08bad66
KnowsCount <knowscount@gmail.com>
2020-11-24 11:09:35 +0800

    beta.24112020@uploadImgServerPro

--- service/app.js
+++ service/app.js
@@ -53,10 +53,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// app.use(logger("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 if (app.get("env") === "development") {
 	app.use(function (req, res, next) {
